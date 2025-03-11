250a99f67a9ee2f7b94ecd5b8a2dcfdde4b80129
Manya Singh <114657669+Manya-singh2001@users.noreply.github.com>
2025-03-12 01:32:09 +0530

    Update EDA.sql

--- EDA.sql
+++ EDA.sql
@@ -48,8 +48,6 @@ CREATE TABLE spotify (
 --1. Find the top 3 most-viewed tracks for each artist using window functions.
 --2. Write a query to find tracks where the liveness score is above the average.
 --3. **Use a `WITH` clause to calculate the difference between the highest and lowest energy values for tracks in each album.**
---5. Find tracks where the energy-to-liveness ratio is greater than 1.2.
---6. Calculate the cumulative sum of likes for tracks ordered by the number of views, using window functions.
 
 
 -- EDA 
@@ -186,14 +184,38 @@ WITH ranking_artist AS (
     GROUP BY artist, track
 )
 SELECT * FROM ranking_artist 
+    WHERE rank <=3
 ORDER BY artist, total_view DESC;
 
 --2. Write a query to find tracks where the liveness score is above the average.
---3. **Use a `WITH` clause to calculate the difference between the highest and lowest energy values for tracks in each album.**
---5. Find tracks where the energy-to-liveness ratio is greater than 1.2.
---6. Calculate the cumulative sum of likes for tracks ordered by the number of views, using window functions.
+-- avg liveness = 0.19
+SELECT 
+    track,
+    artist,
+    liveness
+    FROM spotify 
+WHERE liveness > ( SELECT AVG(LIVENESS) FROM spotify) ;
+
+--3. Use a `WITH` clause to calculate the difference between the highest and lowest energy values for tracks in each album.
+
+WITH cte 
+AS 
+(SELECT 
+    album,
+    MAX(energy) as highest_energy,
+    MIN(energy) as lowest_energy
+    FROM spotify
+    GROUP BY 1
+    )
+SELECT 
+album,
+highest_energy - lowest_energy as energy_diff
+FROM cte
+ORDER BY 2 ;
+    
 
 
+-----------------------------------------------------------------------------------------
 
 
 
